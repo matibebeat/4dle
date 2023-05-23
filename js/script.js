02d4376e632ae9a00fea80caa8c22ed9bc708f4b
matibebeat <mathislair@gmail.com>
2023-05-23 14:46:53 +0200

    beaucoup de changements

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-const users={
-    "Maxime":["Brown",148,"Brown",175,"right","sport","M",4],
-    "Youness":["Brown",256,"Brown",184,"right","sport","M",1],
-    "Vincent":["Brown",80,"Black",180,"right","sleep","M",3],
-    "Leanna":["Brown",136,"Multicolor",173,"right","music","F",4],
-    "Martin":["Green",246,"Brown",178,"left","games","M",5],
-    "Damien":["Brown",0,"Black",180,"right","sport","M",2],
-    "Melissa":["Brown",125,"Black",161,"right","sleep","F",4],
-    "Charles":["Brown",95,"Brown",175,"right","work","M",3],
-    "Audrey":["Bicolor",250,"Multicolor",163,"right","eat","F",1],
-    "Mathieu":["Green",260,"Black",173,"left","sport","M",4],
-    "Tony":["Brown",0,"Brown",173,"right","games","M",5],
-    "Vianney":["Brown",37,"Grey",183,"right","games","M",4],
-    "Romain":["Green",0,"Brown",171,"Right","sport","M",4],
-    "Mathis":["Brown",168,"Brown",168,"right","sport","M",1 ],
-    "Eyfeline":["Brown",1,"Blond",160,"right","games","F",5],
-    "Kevin":["Brown",270,"Brown",155,"right","games","M",5],
-    "Farès":["Green",240,"Brown",180,"right","sport","M",2],
+const users = {
+    "Maxime": ["Brown", 148, "Brown", 175, "right", "sport", "M", 4],
+    "Youness": ["Brown", 256, "Brown", 184, "right", "sport", "M", 1],
+    "Vincent": ["Brown", 80, "Black", 180, "right", "sleep", "M", 3],
+    "Leanna": ["Brown", 136, "Multicolor", 173, "right", "music", "F", 4],
+    "Martin": ["Green", 246, "Brown", 178, "left", "games", "M", 5],
+    "Damien": ["Brown", 0, "Black", 180, "right", "sport", "M", 2],
+    "Melissa": ["Brown", 125, "Black", 161, "right", "sleep", "F", 4],
+    "Charles": ["Brown", 95, "Brown", 175, "right", "work", "M", 3],
+    "Audrey": ["Bicolor", 250, "Multicolor", 163, "right", "eat", "F", 1],
+    "Mathieu": ["Green", 260, "Black", 173, "left", "sport", "M", 4],
+    "Tony": ["Brown", 0, "Brown", 173, "right", "games", "M", 5],
+    "Vianney": ["Brown", 37, "Grey", 183, "right", "games", "M", 4],
+    "Romain": ["Green", 0, "Brown", 171, "Right", "sport", "M", 4],
+    "Mathis": ["Brown", 168, "Brown", 168, "right", "sport", "M", 1],
+    "Eyfeline": ["Brown", 1, "Blond", 160, "right", "games", "F", 5],
+    "Kevin": ["Brown", 270, "Brown", 155, "right", "games", "M", 5],
+    "Farès": ["Green", 240, "Brown", 180, "right", "sport", "M", 2],
 }
 
-var tryes=0;
-document.addEventListener("DOMContentLoaded", function() {
+var tryes = 0;
+document.addEventListener("DOMContentLoaded", function () {
     var datalist = document.getElementById("names");
     var users_list = Object.keys(users);
-    
-    users_list.forEach(function(element) {
-      datalist.appendChild(new Option(element, element));
+
+    users_list.forEach(function (element) {
+        datalist.appendChild(new Option(element, element));
     });
-  });
+});
 const green = "rgb(0, 255, 0)";
 
 var names = Object.keys(users);
@@ -58,76 +58,97 @@ function makeaguess() {
     var td_name = document.createElement("td");
 
     td_eyes.innerHTML = user[0];
-    if(user[0]==Choice_user_data[0]){
-        td_eyes.style.backgroundColor=green;
+    if (user[0] == Choice_user_data[0]) {
+        td_eyes.style.backgroundColor = green;
     }
-    else{
-        td_eyes.style.backgroundColor="red";
+    else {
+        td_eyes.style.backgroundColor = "red";
     }
 
     td_rank.innerHTML = user[1];
-    if(user[1]==Choice_user_data[1]){
-        td_rank.style.backgroundColor=green;
-    }   
-    else{
-        td_rank.style.backgroundColor="red";
+    if (user[1] == Choice_user_data[1]) {
+        td_rank.style.backgroundColor = green;
+    }
+    else {
+        td_rank.style.backgroundColor = "red";
+        td_rank.className = "up";
     }
 
     td_hair.innerHTML = user[2];
-    if(user[2]==Choice_user_data[2]){
-        td_hair.style.backgroundColor=green;
+    if (user[2] == Choice_user_data[2]) {
+        td_hair.style.backgroundColor = green;
     }
-    else{
-        td_hair.style.backgroundColor="red";
+    else if (user[2] == "Multicolor") {
+        var gradientColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+        ;
+
+        var gradient = 'radial-gradient(circle, ';
+        for (var j = 0; j < gradientColors.length; j++) {
+            gradient += gradientColors[j] + ' ' + (j * (100 / (gradientColors.length - 1))) + '%';
+            if (j !== gradientColors.length - 1) {
+                gradient += ', ';
+            }
+        }
+        gradient += ')';
+        td_hair.style.backgroundImage = gradient;
+    }
+    else {
+        td_hair.style.backgroundColor = "red";
     }
     //here we want to make the color change if the user is right or wrong
     td_height.innerHTML = user[3];
-    if(user[3]==Choice_user_data[3]){
-        td_height.style.backgroundColor=green;
+    if (user[3] == Choice_user_data[3]) {
+        td_height.style.backgroundColor = green;
     }
-    else{
-        const distance = Math.abs(user[3]-Choice_user_data[3]); // Distance par rapport à la hauteur cible (170cm)
+    else {
+        const distance = Math.abs(user[3] - Choice_user_data[3]); // Distance par rapport à la hauteur cible (170cm)
 
         // Calcul de la couleur en fonction de la distance
         const red = Math.floor(255 * (distance / 30));
         const green = Math.floor(255 - (255 * (distance / 30)));
         console.log(red, green);
-        td_height.style.backgroundColor="rgb(" + red + "," + green + ",0)";
+        td_height.style.backgroundColor = "rgb(" + red + "," + green + ",0)";
     }
 
     td_gamer.innerHTML = user[7];
-    if(user[7]==Choice_user_data[7]){
-        td_gamer.style.backgroundColor=green;
+    if (user[7] == Choice_user_data[7]) {
+        td_gamer.style.backgroundColor = green;
     }
-    else{
-        td_gamer.style.backgroundColor="red";
+    else {
+        td_gamer.style.backgroundColor = "red";
     }
 
     td_hand.innerHTML = user[4];
-    if(user[4]==Choice_user_data[4]){
-        td_hand.style.backgroundColor=green;
+    if (user[4] == Choice_user_data[4]) {
+        td_hand.style.backgroundColor = green;
     }
-    else{
-        td_hand.style.backgroundColor="red";
+    else {
+        td_hand.style.backgroundColor = "red";
     }
 
     td_hobby.innerHTML = user[5];
-    if(user[5]==Choice_user_data[5]){
-        td_hobby.style.backgroundColor=green;
+    if (user[5] == Choice_user_data[5]) {
+        td_hobby.style.backgroundColor = green;
     }
-    else{
-        td_hobby.style.backgroundColor="red";
+    else {
+        td_hobby.style.backgroundColor = "red";
     }
 
     td_sex.innerHTML = user[6];
-    if(user[6]==Choice_user_data[6]){
-        td_sex.style.backgroundColor=green;
+    if (user[6] == Choice_user_data[6]) {
+        td_sex.style.backgroundColor = green;
     }
-    else{
-        td_sex.style.backgroundColor="red";
+    else {
+        td_sex.style.backgroundColor = "red";
     }
     td_name.innerHTML = name.value;
-    row.append(td_name,td_eyes,td_rank,td_hair,td_height,td_gamer,td_hand,td_hobby,td_sex);
+    if (name.value == Choice_user) {
+        td_name.style.backgroundColor = green;
+    }
+    else {
+        td_name.style.backgroundColor = "red";
+    }
+    row.append(td_name, td_eyes, td_rank, td_hair, td_height, td_gamer, td_hand, td_hobby, td_sex);
     table_body[0].append(row);
     console.log(user);
     console.log(Choice_user);
@@ -137,11 +158,11 @@ function makeaguess() {
 
 
 
-        
 
 
 
-    
+
+
     var title = document.getElementById("title_end");
     tryes++;
     title.innerHTML = tryes;
